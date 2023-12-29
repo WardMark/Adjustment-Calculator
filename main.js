ad4c4d302461d82680488dfcefb97aa4253f08ae
WardMark <mark@drawkram.com>
2023-12-29 11:12:43 -0600

    Got everything working!

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
 function systemCalculate() {
-  let x = document.getElementById('systems');
-  x.innerHTML = "Full thickener hit: ";//lines * tank level;
-  x.style.display = 'block';
+  let number1 = parseFloat(document.getElementById('n1').value);
+  let number2 = parseFloat(document.getElementById('n2').value);
+  let adjT = (number1 + number2) * 0.005;
+  let adjAG = (number1 + number2) * 0.0005;
+  let adjAO = (adjAG * 128).toFixed(1);
+  document.getElementById('systems').innerText = "A full hit of thickener is " + adjT.toFixed(2) + " ga.. One full amine hit is " + adjAO + " ounces.";
 }
 
 function systemReset() {
-  x.style.display = 'none';
+  document.getElementById('systems').innerText = '';
 }
 
 function toteCalculate() {
-
+  let t = parseFloat(document.getElementById('tote').value);
+  let toteT = t * 0.005;
+  let toteA = t * 0.0005;
+  let toteAO = (toteA * 128).toFixed(1);
+  document.getElementById('toteDiv').innerText = "A full hit of thickener is " + toteT.toFixed(2) + " ga.. One full amine hit is " + toteAO + " ounces.";
 }
  function toteReset() {
-
+  document.getElementById('toteDiv').innerText = '';
  }
  

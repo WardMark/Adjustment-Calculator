3332a1e0de53fd3f4184da654858ccf14bc97ba6
WardMark <mark@drawkram.com>
2023-12-28 21:36:58 -0600

    Separated the JS scripts & changed form

---
+++ main.js
@@ -0,0 +1,19 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+function systemCalculate() {
+  let x = document.getElementById('systems');
+  x.innerHTML = "Full thickener hit: ";//lines * tank level;
+  x.style.display = 'block';
+}
+
+function systemReset() {
+  x.style.display = 'none';
+}
+
+function toteCalculate() {
+
+}
+ function toteReset() {
+
+ }
+ 
